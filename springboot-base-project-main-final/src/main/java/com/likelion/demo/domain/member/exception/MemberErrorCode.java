ec65342aa71b325248268bc0d40696590b70538e
EunJi <univ217@naver.com>
2025-05-22 07:28:17 +0900

    feat/2 회원가입 기능 추가

--- springboot-base-project-main-final/src/main/java/com/likelion/demo/domain/member/exception/MemberErrorCode.java
+++ springboot-base-project-main-final/src/main/java/com/likelion/demo/domain/member/exception/MemberErrorCode.java
@@ -8,7 +8,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum MemberErrorCode implements BaseResponseCode {
     //404 유저가 존재하지 않음
-    MEMBER_NOT_FOUND_404("MEMBER_NOT_FOUND_404",404,"해당 유저는 존재하지 않습니다.");
+    MEMBER_NOT_FOUND_404("MEMBER_NOT_FOUND_404",404,"해당 유저는 존재하지 않습니다."),
+    DUPLICATE_EMAIL("MEMBER_400_1", 400, "이미 사용 중인 아이디입니다."),
+    PASSWORD_NOT_MATCH("MEMBER_400_2", 400, "비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+
 
     private final String code;
     private final int httpStatus;
