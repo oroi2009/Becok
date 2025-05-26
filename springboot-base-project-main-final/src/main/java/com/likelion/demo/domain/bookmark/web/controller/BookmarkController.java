f294f37f7af8edc017b323923cd8ad34c4a11bd1
khyaejin <olivia019182@naver.com>
2025-05-27 04:51:37 +0900

    ✨ Feat: 북마크 controller 로직 완성

--- springboot-base-project-main-final/src/main/java/com/likelion/demo/domain/bookmark/web/controller/BookmarkController.java
+++ springboot-base-project-main-final/src/main/java/com/likelion/demo/domain/bookmark/web/controller/BookmarkController.java
@@ -1,13 +1,31 @@
 package com.likelion.demo.domain.bookmark.web.controller;
 
+import com.likelion.demo.domain.bookmark.service.ContestBookmarkService;
+import com.likelion.demo.domain.bookmark.service.ProgramBookmarkService;
 import com.likelion.demo.domain.bookmark.web.dto.BookmarkToggleReq;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.likelion.demo.domain.bookmark.web.dto.BookmarkToggleRes;
+import com.likelion.demo.global.response.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/bookmark")
+@RestController
+@RequestMapping("/bookmark")
+@RequiredArgsConstructor
 public class BookmarkController {
 
-}
+    private final ContestBookmarkService contestBookmarkService;
+    private final ProgramBookmarkService programBookmarkService;
 
+    @PostMapping("/{memberId}")
+    public SuccessResponse<BookmarkToggleRes> toggleBookmark(@PathVariable Long memberId,
+                                                             @RequestBody BookmarkToggleReq request) {
+        boolean isOn = switch (request.getType()) {
+            case "program" -> programBookmarkService.toggle(request.getContentId(), memberId);
+            case "contest" -> contestBookmarkService.toggle(request.getContentId(), memberId);
+            default -> throw new IllegalArgumentException("Invalid bookmark type: " + request.getType());
+        };
+
+        BookmarkToggleRes response = new BookmarkToggleRes(isOn ? "on" : "off");
+        return SuccessResponse.created(response);
+    }
+}
