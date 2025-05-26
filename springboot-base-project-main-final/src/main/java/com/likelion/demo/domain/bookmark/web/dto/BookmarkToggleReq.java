e5388e2364ab3dd4be92bbf7e11c67f4b43a3424
khyaejin <olivia019182@naver.com>
2025-05-27 04:42:10 +0900

    ✨ Feat: 북마크 Dto 완성

--- springboot-base-project-main-final/src/main/java/com/likelion/demo/domain/bookmark/web/dto/BookmarkToggleReq.java
+++ springboot-base-project-main-final/src/main/java/com/likelion/demo/domain/bookmark/web/dto/BookmarkToggleReq.java
@@ -1,4 +1,9 @@
 package com.likelion.demo.domain.bookmark.web.dto;
 
-public class BookmarkTogleReq {
+import lombok.Getter;
+
+@Getter
+public class BookmarkToggleReq {
+    private String type; // contest(공모전) or program(비교과)
+    private Long contentId; // 해당 공모전/비교과의 고유 id
 }
