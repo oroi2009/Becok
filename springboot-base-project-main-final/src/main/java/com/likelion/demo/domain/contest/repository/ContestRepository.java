8639b569de60fe7e0dfb7c32af00fb4f3a224a64
EunJi <univ217@naver.com>
2025-05-27 13:47:10 +0900

    공모전 크롤링 코드 상세url, 중복저장 안되게 수정

--- springboot-base-project-main-final/src/main/java/com/likelion/demo/domain/contest/repository/ContestRepository.java
+++ springboot-base-project-main-final/src/main/java/com/likelion/demo/domain/contest/repository/ContestRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ContestRepository extends JpaRepository<Contest, Long> {
-
+    boolean existsByName(String name);
     List<Contest> findTop5ByOrderByHitsDesc();
 }
 
